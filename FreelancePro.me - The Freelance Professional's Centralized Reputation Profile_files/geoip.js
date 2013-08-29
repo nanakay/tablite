2ef9b3891049cd61ccf2b84d7e60a203a20c78d9
JFunu <jfunu79@gmail.com>
2013-08-29 00:56:36 +0000

    Fast tracked website designed

---
+++ FreelancePro.me - The Freelance Professional's Centralized Reputation Profile_files/geoip.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'GH'; }
+function geoip_country_name() { return 'Ghana'; }
+function geoip_city()         { return 'Accra'; }
+function geoip_region()       { return '01'; }
+function geoip_region_name()  { return 'Greater Accra'; }
+function geoip_latitude()     { return '5.55'; }
+function geoip_longitude()    { return '-0.2167'; }
+function geoip_postal_code()  { return ''; }
+function geoip_area_code()    { return ''; }
+function geoip_metro_code()   { return ''; }
